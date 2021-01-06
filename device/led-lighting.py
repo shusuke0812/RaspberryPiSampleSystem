40eba2b55fc12425733d20859b26cf45e0032026
shusuke0812_raspi <shu.ota0812@gmail.com>
2021-01-06 16:34:26 +0900

    [add]gitignoreファイルとled点滅プログラムを追加

---
+++ device/led-lighting.py
@@ -0,0 +1,25 @@
+"""
+2つのLEDを点滅させるプログラム
+"""
+import RPi.GPIO as GPIO
+import time
+
+# カウンタ
+num = 0
+# LEDのGPIO番号
+green_led_pin = 2
+yellow_led_pin = 3
+# スリープ時間[s]
+sleep_time = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(green_led_pin, GPIO.OUT)
+
+while(num < 10):
+    GPIO.output(green_led_pin, True)
+    time.sleep(sleep_time)
+    GPIO.outpit(green_led_pin)
+    time.sleep(sleep_time)
+    num += 1
+
+GPIO.cleanup()
