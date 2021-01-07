c4dc4c1a230dbc7ff893dcc77665f36be07b24a5
shusuke0812 <shu.ota0812@gmail.com>
2021-01-07 17:44:47 +0900

    [fix]LEDを点滅させるコードを関数に分離

--- device/led-lighting.py
+++ device/led-lighting.py
@@ -11,15 +11,21 @@ green_led_pin = 2
 yellow_led_pin = 3
 # スリープ時間[s]
 sleep_time = 1
-
+# GPIOの設定
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(green_led_pin, GPIO.OUT)
 
+# 関数
+def led_lighting(gpio: int, sleep_time: int):
+    GPIO.output(gpio, True)
+    time.sleep(sleep_time)
+    GPIO.output(gpio, False)
+    time.sleep(sleep_time)
+
+# メイン処理
 while(num < 10):
-    GPIO.output(green_led_pin, True)
-    time.sleep(sleep_time)
-    GPIO.output(green_led_pin, False)
-    time.sleep(sleep_time)
+    led_lighting(green_led_pin, sleep_time)
     num += 1
 
 GPIO.cleanup()
+
